8b4d54ae27e9bee161166d3b9cf64d661ad8f28f
KBlacksmith <78935468+KBlacksmith@users.noreply.github.com>
2021-05-17 11:45:50 -0500

    Add files via upload

---
+++ PIA_Lab_POO/src/pia_lab_poo/Usuario.java
@@ -0,0 +1,37 @@
+
+package pia_lab_poo;
+
+import java.util.Scanner;
+
+
+public class Usuario implements Permiso{
+    private String userName, password;
+    public Usuario(String userName, String password){
+        this.userName = userName; 
+        this.password = password;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String nivel(){
+        return "Empleado"; 
+    }
+    @Override
+    public void crearUsuario() {
+        System.out.println(this.userName+" no tiene permiso de crear nuevos usuarios");
+    }
+
+    @Override
+    public void eliminarUsuario() {
+        System.out.println(this.userName+" no tiene permiso de eliminar usuarios"); 
+    }
+}
